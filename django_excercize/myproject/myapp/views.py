9cb3fee82f970b9362b8138013632aa08b2e54d8
Daisuke Osanai <dsk.osanai@gmail.com>
2024-09-16 08:27:38 +0900

    add urlPattern

--- django_excercize/myproject/myapp/views.py
+++ django_excercize/myproject/myapp/views.py
@@ -1,3 +1,9 @@
+from django.http import HTTPResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    text = '''Hogehoge'''
+    return HTTPResponse(text)
+
+

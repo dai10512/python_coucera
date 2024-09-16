6040d49b51b768c55e0b87c1bfc5d28f75dd45d4
Daisuke Osanai <dsk.osanai@gmail.com>
2024-09-16 10:05:09 +0900

    add parameter

--- django_excercize/myproject/myapp/views.py
+++ django_excercize/myproject/myapp/views.py
@@ -1,9 +1,13 @@
-from django.http import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    text = '''Hogehoge'''
-    return HTTPResponse(text)
-
-
+def drinks(request,drink_name):
+    drinkDic = {
+        'mocha':'type of coffee',
+        'tea':'type of beverage',
+        'lemonade' : 'type of refreshment',
+    }
+    choice_of_drink = drinkDic[drink_name]
+    title = f"<h2> {drink_name} </h2>"
+    return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)

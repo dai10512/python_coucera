0e475758017d79427f98961313534a227d225f30
Daisuke Osanai <dsk.osanai@gmail.com>
2024-09-29 15:38:05 +0900

    add Restaurant menu API using serialization project

---
+++ django_api/restaurant-menu-api-using-serialization/LittleLemon/LittleLemonDRF/urls.py
@@ -0,0 +1,9 @@
+from .models import MenuItem
+from rest_framework import serializers
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('menu-items', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+]

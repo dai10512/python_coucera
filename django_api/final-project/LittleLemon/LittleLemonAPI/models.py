df11a6cfa618ec43fe6553c8730623296a1ee94c
Daisuke Osanai <dsk.osanai@gmail.com>
2024-10-08 16:31:08 +0900

    Set model

--- django_api/final-project/LittleLemon/LittleLemonAPI/models.py
+++ django_api/final-project/LittleLemon/LittleLemonAPI/models.py
@@ -1,28 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    slug = models.SlugField()
+    title = models.CharField(max_length=255, db_index=True)
 
 
 class MenuItem(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    featured = models.BooleanField(db_index=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
 
 
 class Cart(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.PROTECT)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('user', 'menuitem')
 
 
 class Order(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    delivery_crew = models.ForeignKey(
+        User,
+        related_name='delivery_crew',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    status = models.IntegerField(db_index=True, default=0)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateTimeField(db_index=True)
 
 
 class OrderItem(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('order', 'menuitem')

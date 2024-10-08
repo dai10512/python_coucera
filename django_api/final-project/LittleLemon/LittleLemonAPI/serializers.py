5181a1653122a1551a3b14cfbf44ec31812c4a0a
Daisuke Osanai <dsk.osanai@gmail.com>
2024-10-08 19:27:41 +0900

    add serializer class

--- django_api/final-project/LittleLemon/LittleLemonAPI/serializers.py
+++ django_api/final-project/LittleLemon/LittleLemonAPI/serializers.py
@@ -1,2 +1,33 @@
-# from django.urls import path
-# from . import views
+from rest_framework import serializers
+from .models import MenuItem, Category, Cart, Order, OrderItem
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'

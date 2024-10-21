4ab4813dcd8886468310da53675c6291415528b0
Daisuke Osanai <dsk.osanai@gmail.com>
2024-10-21 13:02:00 +0900

    fix authentication

--- django_api/final-project/LittleLemon/LittleLemon/urls.py
+++ django_api/final-project/LittleLemon/LittleLemon/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('LittleLemonAPI.urls')),
     path("", include("djoser.urls.authtoken")),
+    path(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]

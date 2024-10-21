0e0a26a420ae746fbb0d9aa32f86fc6ba0fea285
Daisuke Osanai <dsk.osanai@gmail.com>
2024-10-21 23:18:45 +0900

    fix setting

--- django_api/Little Lemon API project/LittleLemon/LittleLemon/urls.py
+++ django_api/Little Lemon API project/LittleLemon/LittleLemon/urls.py
@@ -21,7 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('LittleLemonAPI.urls')),
-    path("", include("djoser.urls.authtoken")),
-    path(r"^api-auth/", include("rest_framework.urls",
-                                namespace="rest_framework")),
+    path("auth/", include("djoser.urls.authtoken")),
 ]
